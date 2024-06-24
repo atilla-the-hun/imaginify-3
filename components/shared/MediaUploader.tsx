afcb6ed91aaa72931e9340fbe6d38ebf2ea07f22
atilla-the-hun <zoltan.abonyi5@gmail.com>
2024-06-24 10:37:47 +0200

    first commit

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="pixel-craft"
       options={{
         multiple: false,
         resourceType: "image",
