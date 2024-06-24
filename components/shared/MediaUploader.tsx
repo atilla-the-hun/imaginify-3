a17b9c8464a761cd7e8e2216ab3cdb2b4b2fa091
atilla-the-hun <zoltan.abonyi5@gmail.com>
2024-06-24 10:59:06 +0200

    env update

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="pixel-craft"
+      uploadPreset="jsm_pixelcraft"
       options={{
         multiple: false,
         resourceType: "image",
